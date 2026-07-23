e532776e9718acb758b5bc6f2612d5b467719472
LucSousa21 <souza.lucas1805@gmail.com>
2026-07-22 22:21:02 -0300

    docs: adiciona título, descrição e versão da API

--- app/main.py
+++ app/main.py
@@ -2,5 +2,9 @@ from fastapi import FastAPI
 from .routes import router
 
 # Inicializa a aplicação FastAPI e registra as rotas da API.
-app = FastAPI()
+app = FastAPI(
+    title="API Português",
+    description="API para consultar assuntos de Portguês",
+    version="1.0.0"
+)
 app.include_router(router)
